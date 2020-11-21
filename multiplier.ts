c559606a072b92ca33668db6dc4e60aa5c28f350
bonasunu <bonaventurasunu@gmail.com>
2020-11-22 00:45:38 +0700

    create calculator function

--- multiplier.ts
+++ multiplier.ts
@@ -2,4 +2,27 @@ const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b)
 }
 
-multiplicator(2, 4, 'The result is:')
+// multiplicator(2, 4, 'The result is:')
+
+type Operation = 'multiply' | 'divide' | 'add'
+type Result = number
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+  switch (op) {
+    case 'multiply':
+      return a * b
+    case 'add':
+      return a + b
+    case 'divide':
+      if (b === 0) throw new Error("Can't divide by 0!")
+      return a / b
+    default:
+      throw new Error('Operation is not multiply, add or divide!')
+  }
+}
+
+try {
+  console.log(calculator(1, 5, 'divide'))
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message)
+}
