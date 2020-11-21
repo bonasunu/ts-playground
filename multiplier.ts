ddeb82142888e4f814c2974ac5838720c49bf7c9
bonasunu <bonaventurasunu@gmail.com>
2020-11-22 01:01:38 +0700

    refactor code and add @type/node

--- multiplier.ts
+++ multiplier.ts
@@ -3,26 +3,7 @@ const multiplicator = (a: number, b: number, printText: string) => {
 }
 
 // multiplicator(2, 4, 'The result is:')
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
 
-type Operation = 'multiply' | 'divide' | 'add'
-type Result = number
-
-const calculator = (a: number, b: number, op: Operation): Result => {
-  switch (op) {
-    case 'multiply':
-      return a * b
-    case 'add':
-      return a + b
-    case 'divide':
-      if (b === 0) throw new Error("Can't divide by 0!")
-      return a / b
-    default:
-      throw new Error('Operation is not multiply, add or divide!')
-  }
-}
-
-try {
-  console.log(calculator(1, 5, 'divide'))
-} catch (e) {
-  console.log('Something went wrong, error message: ', e.message)
-}
+multiplicator(a, b, 'The result is: ')
